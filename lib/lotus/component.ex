b0b4cc38191345792750f3a4424d8277dcd89bff
Mafinar Khan <mafinar@gmail.com>
2021-09-17 23:09:32 -0400

    Credo induced OCD :/

--- lib/lotus/component.ex
+++ lib/lotus/component.ex
@@ -1,4 +1,7 @@
 defmodule Lotus.Component do
+  @moduledoc """
+  Base component for Lotus, encapsulating mixins and prop modules
+  """
   defmacro __using__(_) do
     quote do
       use Surface.Component
